5e23b5ed5486a0da44224b49b998df7b0918d950
carlosperate <carlosperate@users.noreply.github.com>
2018-04-23 11:45:53 +0100

    Workaround to Yotta dependencies issues with pip v10
    More info in https://github.com/carlosperate/microbit-dev-env/issues/5

--- install_yotta.sh
+++ install_yotta.sh
@@ -7,5 +7,8 @@ sudo apt-get install -y python-setuptools python-dev cmake build-essential ninja
 pretty_echo "Installing pip..."
 sudo easy_install pip
 sudo pip install -U pip
+# Temporary workaround for https://github.com/carlosperate/microbit-dev-env/issues/5
+pretty_echo "Downgrading pip to v9 due to package issues with v10..."
+sudo -H pip install pip==9.0.3
 pretty_echo "Installing yotta..."
 sudo -H pip install yotta
